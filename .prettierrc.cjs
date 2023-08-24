f275df26f8462fbd2dfeca933f2c0216f269bcfe
roritopra <1193102598@u.icesi.edu.co>
2023-08-24 09:47:29 -0500

    [ADD] Added prettier format

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: true,
+    plugins: [require('prettier-plugin-astro')],
+    overrides: [
+      {
+        files: '**/*.astro',
+        options: {
+          parser: 'astro',
+        },
+      },
+    ],
+  }
+  
